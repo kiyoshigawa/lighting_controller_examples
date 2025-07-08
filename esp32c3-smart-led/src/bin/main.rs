c17e059884c831ec3115be93d0af02245bbfdacd
kiyoshigawa <tim@twa.ninja>
2025-07-07 20:00:24 -0600

    Rework lighting_controller code to work with new hal

--- esp32c3-smart-led/src/bin/main.rs
+++ esp32c3-smart-led/src/bin/main.rs
@@ -1,103 +1,49 @@
 #![no_std]
 #![no_main]
 
-use esp_hal::clock::CpuClock;
-use esp_hal::main;
+use esp_backtrace as _;
 use esp_hal::time::{Duration, Instant};
-
-#[panic_handler]
-fn panic(_: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
+use esp_hal::{clock::CpuClock, main, rmt::Rmt, time::Rate};
+use esp_hal_smartled::{smart_led_buffer, SmartLedsAdapter};
+use esp_println::println;
+use lc::animations::{Animatable, Animation};
+use lc::{default_animations, LightingController, LogicalStrip};
+use lighting_controller as lc;
+use smart_leds::{brightness, colors::*, gamma, SmartLedsWrite as _};
 
 #[main]
 fn main() -> ! {
-    // generator version: 0.3.1
-
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
 
+    let frequency = Rate::from_mhz(80);
+    let rmt = Rmt::new(peripherals.RMT, frequency).expect("Failed to initialize RMT0");
+
+    let mut led_strip =
+        SmartLedsAdapter::new(rmt.channel0, peripherals.GPIO9, smart_led_buffer!(16));
+    const STRIP_BRIGHTNESS: u8 = 150;
+
+    let frame_rate = embedded_time::rate::Extensions::Hz(144);
+    let frame_rate_in_ticks = Duration::from_micros(6900u64);
+    let color_buffer = &mut [BLACK; 16];
+    let mut ls = LogicalStrip::new(color_buffer);
+    let a1 = &mut Animation::<16>::new(default_animations::ANI_TEST, frame_rate);
+    let animations: [&mut dyn Animatable; 1] = [a1];
+    let mut lc = LightingController::new(animations, frame_rate);
+
+    println!("Peripherals configured, entering main loop.");
+
+    let mut last_update_time = Instant::now();
     loop {
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        if Instant::now() > (last_update_time + frame_rate_in_ticks) {
+            last_update_time = Instant::now();
+            lc.update(&mut ls);
+            led_strip
+                .write(brightness(
+                    gamma(ls.color_buffer.iter().copied()),
+                    STRIP_BRIGHTNESS,
+                ))
+                .unwrap();
+        }
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
 }
-
-// #![no_std]
-// #![no_main]
-
-// use esp32c3_hal::{
-//     clock::ClockControl, peripherals::Peripherals, prelude::*, pulse_control::ClockSource,
-//     systimer::SystemTimer, timer::TimerGroup, PulseControl, Rtc, IO,
-// };
-// use esp_backtrace as _;
-// use esp_hal_smartled::{smartLedAdapter, SmartLedsAdapter};
-// use lc::animations::{Animatable, Animation};
-// use lc::{default_animations, LightingController, LogicalStrip};
-// use lighting_controller as lc;
-// use smart_leds::{brightness, colors::*, gamma, SmartLedsWrite};
-
-// #[entry]
-// fn main() -> ! {
-//     let peripherals = Peripherals::take();
-//     let mut system = peripherals.SYSTEM.split();
-//     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-
-//     // Disable the watchdog timers. For the ESP32-C3, this includes the Super WDT,
-//     // the RTC WDT, and the TIMG WDTs.
-//     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-//     let timer_group0 = TimerGroup::new(
-//         peripherals.TIMG0,
-//         &clocks,
-//         &mut system.peripheral_clock_control,
-//     );
-//     let mut wdt0 = timer_group0.wdt;
-//     let timer_group1 = TimerGroup::new(
-//         peripherals.TIMG1,
-//         &clocks,
-//         &mut system.peripheral_clock_control,
-//     );
-//     let mut wdt1 = timer_group1.wdt;
-//     rtc.swd.disable();
-//     rtc.rwdt.disable();
-//     wdt0.disable();
-//     wdt1.disable();
-
-//     // Set up GPIO peripheral doe use with LED output
-//     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-
-//     // Configure RMT peripheral globally
-//     let pulse = PulseControl::new(
-//         peripherals.RMT,
-//         &mut system.peripheral_clock_control,
-//         ClockSource::APB,
-//         0,
-//         0,
-//         0,
-//     )
-//     .unwrap();
-
-//     let mut led = <smartLedAdapter!(16)>::new(pulse.channel0, io.pins.gpio9);
-
-//     let frame_rate = embedded_time::rate::Extensions::Hz(144);
-//     let frame_rate_in_ticks = SystemTimer::TICKS_PER_SECOND / frame_rate.0 as u64;
-//     let color_buffer = &mut [BLACK; 16];
-//     let mut ls = LogicalStrip::new(color_buffer);
-//     let a1 = &mut Animation::<16>::new(default_animations::ANI_TEST, frame_rate);
-//     let animations: [&mut dyn Animatable; 1] = [a1];
-//     let mut lc = LightingController::new(animations, frame_rate);
-
-//     esp_println::println!("Peripherals configured, entering main loop.");
-
-//     let mut last_update_time = SystemTimer::now();
-//     loop {
-//         if SystemTimer::now() > (last_update_time + frame_rate_in_ticks) {
-//             last_update_time = SystemTimer::now();
-//             lc.update(&mut ls);
-//             led.write(brightness(gamma(ls.color_buffer.iter().copied()), 255))
-//                 .unwrap();
-//         }
-//     }
-// }
