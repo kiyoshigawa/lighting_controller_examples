f1a3b4195374dd45028f37f22198ef4b4d872dc3
kiyoshigawa <tim@twa.ninja>
2025-10-26 17:20:40 -0600

    Added oklab rainbow for more uniform light, and button swap

--- esp32c3-smart-led-multi-pin/src/bin/main.rs
+++ esp32c3-smart-led-multi-pin/src/bin/main.rs
@@ -11,6 +11,7 @@ use lc::animations::{Animatable, Animation, RainbowDir};
 use lc::{utility::default_translation_array, LightingController, LogicalStrip};
 use lighting_controller::default_animations::ANI_DEFAULT;
 use lighting_controller::{self as lc, animations};
+use rgb::RGB8;
 use smart_leds::{brightness, colors::*, gamma, SmartLedsWrite as _};
 
 #[main]
@@ -38,6 +39,48 @@ fn main() -> ! {
     const NUM_LEDS: usize = NUM_LEDS_STRIP_CLOSET_WINDOW + NUM_LEDS_STRIP_DOOR_NORTH;
 
     const STRIP_BRIGHTNESS: u8 = 255;
+
+    const BLACK_RAINBOW: &[RGB8] = &[BLACK];
+    const TYPICAL_RGB_RAINBOW: &[RGB8] = &[RED, YELLOW, GREEN, DARK_BLUE, DARK_MAGENTA];
+    const OKLCH_RAINBOW: &[RGB8] = &[
+        RGB8 { r: 179, g: 005, b: 094 },
+        RGB8 { r: 179, g: 005, b: 094 },
+        RGB8 { r: 217, g: 031, b: 078 },
+        RGB8 { r: 252, g: 059, b: 041 },
+        RGB8 { r: 252, g: 059, b: 041 },
+        RGB8 { r: 246, g: 071, b: 031 },
+        RGB8 { r: 240, g: 081, b: 023 },
+        RGB8 { r: 234, g: 090, b: 018 },
+        RGB8 { r: 228, g: 097, b: 017 },
+        RGB8 { r: 228, g: 097, b: 017 },
+        RGB8 { r: 218, g: 108, b: 000 },
+        RGB8 { r: 207, g: 117, b: 000 },
+        RGB8 { r: 196, g: 125, b: 000 },
+        RGB8 { r: 184, g: 132, b: 019 },
+        RGB8 { r: 184, g: 132, b: 019 },
+        RGB8 { r: 171, g: 140, b: 000 },
+        RGB8 { r: 150, g: 150, b: 000 },
+        RGB8 { r: 118, g: 160, b: 000 },
+        RGB8 { r: 059, g: 170, b: 024 },
+        RGB8 { r: 059, g: 170, b: 024 },
+        RGB8 { r: 000, g: 157, b: 106 },
+        RGB8 { r: 000, g: 132, b: 146 },
+        RGB8 { r: 000, g: 099, b: 163 },
+        RGB8 { r: 002, g: 063, b: 155 },
+        RGB8 { r: 002, g: 063, b: 155 },
+        RGB8 { r: 055, g: 051, b: 157 },
+        RGB8 { r: 082, g: 039, b: 150 },
+        RGB8 { r: 102, g: 025, b: 136 },
+        RGB8 { r: 119, g: 003, b: 115 },
+        RGB8 { r: 139, g: 004, b: 108 },
+        RGB8 { r: 159, g: 005, b: 101 },
+    ];
+
+    let rainbows = [
+        &OKLCH_RAINBOW[..],
+        &TYPICAL_RGB_RAINBOW[..],
+        &BLACK_RAINBOW[..],
+    ];
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
@@ -80,14 +123,14 @@ fn main() -> ! {
     let color_buffer = &mut [BLACK; NUM_LEDS];
     let mut ls = LogicalStrip::new(color_buffer);
 
+    let mut rainbow_iter = rainbows.iter().cycle().copied();
+    let initial_rainbow = rainbow_iter.next().expect("Iterates forever.");
+
     // closet wall
     let a1 = &mut Animation::<NUM_LEDS_CLOSET_WALL>::new(ANI_DEFAULT, frame_rate)
         .set_translation_array(default_translation_array(START_CLOSET_INDEX))
         // .set_bg_rainbow(&[RED, DARK_RED], true) //debug colors different for each wall
-        .set_bg_rainbow(
-            &[RED, YELLOW, GREEN, DARK_BLUE, DARK_MAGENTA],
-            RainbowDir::Forward,
-        )
+        .set_bg_rainbow(initial_rainbow, RainbowDir::Forward)
         .set_bg_duration_ns(20_000_000_000, frame_rate)
         .set_bg_subdivisions(2);
 
@@ -95,10 +138,7 @@ fn main() -> ! {
     let a2 = &mut Animation::<NUM_LEDS_WINDOW_WALL>::new(ANI_DEFAULT, frame_rate)
         .set_translation_array(default_translation_array(START_WINDOW_INDEX))
         // .set_bg_rainbow(&[BLUE, BLUE_VIOLET], true) //debug colors different for each wall
-        .set_bg_rainbow(
-            &[RED, YELLOW, GREEN, DARK_BLUE, DARK_MAGENTA],
-            RainbowDir::Forward,
-        )
+        .set_bg_rainbow(initial_rainbow, RainbowDir::Forward)
         .set_bg_duration_ns(20_000_000_000, frame_rate)
         .set_bg_subdivisions(2);
 
@@ -106,10 +146,7 @@ fn main() -> ! {
     let a3 = &mut Animation::<NUM_LEDS_DOOR_WALL>::new(ANI_DEFAULT, frame_rate)
         .set_translation_array(core::array::from_fn(|i| (START_NORTH_INDEX - 1) - i))
         // .set_bg_rainbow(&[YELLOW, ORANGE], true) //debug colors different for each wall
-        .set_bg_rainbow(
-            &[RED, YELLOW, GREEN, DARK_BLUE, DARK_MAGENTA],
-            RainbowDir::Forward,
-        )
+        .set_bg_rainbow(initial_rainbow, RainbowDir::Forward)
         .set_bg_duration_ns(20_000_000_000, frame_rate)
         .set_bg_subdivisions(2);
 
@@ -117,10 +154,7 @@ fn main() -> ! {
     let a4 = &mut Animation::<NUM_LEDS_NORTH_WALL>::new(ANI_DEFAULT, frame_rate)
         .set_translation_array(core::array::from_fn(|i| (NUM_LEDS) - 1 - i))
         // .set_bg_rainbow(&[GREEN, DARK_GREEN], true) //debug colors different for each wall
-        .set_bg_rainbow(
-            &[RED, YELLOW, GREEN, DARK_BLUE, DARK_MAGENTA],
-            RainbowDir::Forward,
-        )
+        .set_bg_rainbow(initial_rainbow, RainbowDir::Forward)
         .set_bg_duration_ns(20_000_000_000, frame_rate)
         .set_bg_subdivisions(2);
 
@@ -136,8 +170,12 @@ fn main() -> ! {
         if Instant::now() > (last_button_0_sample_time + BUTTON_DEBOUNCE_TIME) {
             let current_button_0_level = button_0.level();
             if (current_button_0_level == Level::Low) && (last_button_0_level == Level::High) {
-                println!("Press 0!");
-                lc.animations[0].update_bg_duration_ns(5_000_000_000, frame_rate);
+                println!("New Rainbow!");
+                let next_rainbow = rainbow_iter.next().expect("Iterates forever.");
+                lc.animations[0].update_bg_rainbow(next_rainbow, RainbowDir::Forward);
+                lc.animations[1].update_bg_rainbow(next_rainbow, RainbowDir::Forward);
+                lc.animations[2].update_bg_rainbow(next_rainbow, RainbowDir::Forward);
+                lc.animations[3].update_bg_rainbow(next_rainbow, RainbowDir::Forward);
             }
             last_button_0_level = current_button_0_level;
             last_button_0_sample_time = Instant::now();
